5ae7a235b2c07d83c2ba93a459a2a7edfe0469f9
Kevin Porras <kevinportals19@gmail.com>
2024-10-11 23:55:13 -0600

    uimain compiles and runs, fixed typo in Makefile. Changed 0 to O

--- src/uimain.c
+++ src/uimain.c
@@ -5,27 +5,50 @@
 int main(){
   puts("Welcome to the tokenizer machine!");
   puts("---------------------------------");
+  char tokens[10][100];
+  char *toks_ptr;
+  char menu_input; // char used to dictate menu function
+  char idx = 0; // index of current token we are inputing
+  char tok_ptr[10];
 
+  // main_menu loop
   while(1){
 
-    puts("Would you like to tokenize \"t\" , view history \"h\",  or quit \"q\" ");
+    puts("Would you like to tokenize \"t\" , view history \"h\", Find token by I.D. \"i\", or quit \"q\" ");
     puts(">");
 
-    char user_input;
+    menu_input = getchar();
 
-    user_input = getchar();
+    switch(menu_input){
 
-    switch(user_input){
-      case "t":
+      case 't':
         //take user input and tokenize
-        break;
-      case "h":
-        //print history show history
+	puts("Please type what you would like to tokenize");
+	puts(">");
 	break;
-      case"q":
+	
+      case 'h':
+        //print history in linked list
+	puts("Current token history: ");
+	break;
+	
+      case 'i':
+	//find a certain point in the linked list
+	puts("Please enter the id of the token you are looking for");
+	puts(">");
+	break;
+	
+      case 'q':
+	//this signifies the user wants to exit, free tokens
         puts("Thank you for using the tokenize machine, BYE!");
 	goto end;
+	
+      default:
+	puts("Invalid option, Try again! \n");
     }
+
+    int c; // this loop is to clear the buffer so it does not affect the next iteration of the main_menu loop
+    while((c = getchar()) != '\n' && c != EOF){}
 
     
   }
