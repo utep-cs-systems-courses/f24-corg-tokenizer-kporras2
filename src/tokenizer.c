e92fa6c87132e630dc42030871e4f5fa6a75169e
Kevin Porras <kevinportals19@gmail.com>
2024-10-12 20:02:49 -0600

    Updated uimain with nested user input and progress in tokenize.c

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,24 +1,42 @@
 int space_char(char c){
-  return (c == ' ' || c == '\t')? 1 : 0;
+  return ((c == ' ' || c == '\t') && c != '0')? 1 : 0;
 }
 
 int non_space_char(char c){
-  return (c != ' ')? 1 : 0;
+  return ((c != ' ' || c != '\t') && c != '0')? 1 : 0;
 }
 
 char *token_start(char *str){
-  
+  int i = 0;
+  while(space_char(str[i]) && str[i] != '\n'){
+    i++;
+  }
+  return &str[i];
 }
 
 char *token_terminator(char *token){
-  
+  char *start = token_start(token);
+  int i = 0;
+  while(non_space_char(start[i])&& start[i] != '\n'){
+    i++;
+  }
+  return &start[i];
 }
 
 int count_tokens(char *str){
-  
+  int count = 0;
+  char *current = token_start(str);
+  while(*current != '\0'){
+    count++;
+    current = token_terminator(current);
+    current = token_start(current);
+  }
+
+  return count + 1;
 }
 
 char *copy_str(char *inStrm ,short len){
+  char *copied_str;
   
 }
 
--- src/uimain.c
+++ src/uimain.c
@@ -3,19 +3,18 @@
 #include "history.h" //project related declaration
 
 int main(){
-  puts("Welcome to the tokenizer machine!");
-  puts("---------------------------------");
-  char tokens[10][100];
+  printf("Welcome to the tokenizer machine! \n");
+  char token_input[100];
   char *toks_ptr;
   char menu_input; // char used to dictate menu function
+  char index_input[3];
   char idx = 0; // index of current token we are inputing
-  char tok_ptr[10];
 
   // main_menu loop
   while(1){
-
-    puts("Would you like to tokenize \"t\" , view history \"h\", Find token by I.D. \"i\", or quit \"q\" ");
-    puts(">");
+    printf("------------------------------------------\n");
+    printf("Would you like to tokenize \"t\" , view history \"h\", Find token by I.D. \"i\", or quit \"q\"\n ");
+    printf(">");
 
     menu_input = getchar();
 
@@ -23,34 +22,40 @@ int main(){
 
       case 't':
         //take user input and tokenize
-	puts("Please type what you would like to tokenize");
-	puts(">");
+	printf("Please type what you would like to tokenize\n");
+	printf(">");
+
+	fgets(token_input, sizeof(token_input), stdin);
+	//try to tokenize, tell user if sucessfull or not
 	break;
 	
       case 'h':
         //print history in linked list
-	puts("Current token history: ");
+	printf("Current token history:\n\n");
+	//print hitory here
 	break;
 	
       case 'i':
 	//find a certain point in the linked list
-	puts("Please enter the id of the token you are looking for");
-	puts(">");
+	printf("Please enter the id of the token you are looking for\n");
+	printf(">");
+
+	fgets(index_input, sizeof(index_input), stdin);
+	//call id finding
+	
 	break;
 	
       case 'q':
 	//this signifies the user wants to exit, free tokens
-        puts("Thank you for using the tokenize machine, BYE!");
+        printf("Thank you for using the tokenize machine, BYE! \n");
 	goto end;
 	
       default:
-	puts("Invalid option, Try again! \n");
+	printf("Invalid option, Try again! \n");
     }
 
     int c; // this loop is to clear the buffer so it does not affect the next iteration of the main_menu loop
     while((c = getchar()) != '\n' && c != EOF){}
-
-    
   }
 
  end:
