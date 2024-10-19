90dbce23fa8e81427a9b81099276a35f19748aef
Kevin Porras <kevinportals19@gmail.com>
2024-10-18 20:35:47 -0600

    Started History, Completed Tokenize

--- src/history.c
+++ src/history.c
@@ -1,2 +1,33 @@
-void add_history(){
+#include <stdio.h>
+#include <stdlib.h>
+#include "history.h" //project related declerations
+
+
+
+List* init_history(){
+  List *history = (List*)malloc(sizeof(List));
+
+  if(history != NULL){
+    history->root = NULL;
+  }
+
+  return history;
+}
+
+
+
+void add_history(List *list, char *str){
+  //function def goes here
+}
+
+char *get_history(List *list, int id){
+  //function def goes here
+}
+
+void print_history(List *list){
+  //function def goes here
+}
+
+void free_history(List *list){
+  //function def goes here
 }
